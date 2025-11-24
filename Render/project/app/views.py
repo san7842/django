e1a65f44d5bf7a2d599090d70a6069e9695d39b0
san7842 <sandeepjjjjj536@gmail.com>
2025-11-24 19:32:31 +0530

    add

--- Render/project/app/views.py
+++ Render/project/app/views.py
@@ -11,3 +11,17 @@ def myrender(req):
 def home(req):
     return render(req,'home.html')
 
+def about(req):
+    return render(req,'about.html')
+
+def contact(req):
+     return render(req,'contact.html')
+
+def services(req):
+    return render(req,'services.html')
+
+def register(req):
+     return render(req,'register.html')
+
+def login(req):
+    return render(req,'login.html')
--- Render/project/project/urls.py
+++ Render/project/project/urls.py
@@ -21,6 +21,10 @@ from app import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('render/',views.myrender,name='myrender'),
-    path('home/',views.home,name='home')
-    # path('about/',views.about,name='about')
+    path('',views.home,name='home'),
+    path('about/',views.about,name='about'),
+    path('contact/',views.contact,name='contact'),
+    path('services/',views.services,name='services'),
+    path('register/',views.register,name='register'),
+    path('login/',views.login,name='login'),
 ]
