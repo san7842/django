54d40821f5ef53a281f57b0cef9cb57bdf8b9898
san7842 <sandeepjjjjj536@gmail.com>
2025-12-01 19:34:08 +0530

    add

--- Register/project/app/views.py
+++ Register/project/app/views.py
@@ -10,3 +10,11 @@ def register(req):
 def registerdata(req):
      print(req.method)
      print(req.POST)
+     print(req.GET)
+     print(req.FILES)
+     n=req.POST.get('name')
+     e=req.POST.get('email')
+     c=req.POST.get('contact')
+     d=req.POST.get('details')
+     i=req.FILES.get('image')
+     print(n,e,c,d,i)
