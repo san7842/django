1532c6df48866d01fee33563c517f8f971fe2740
san7842 <sandeepjjjjj536@gmail.com>
2025-12-06 19:32:06 +0530

    add

--- RegisterLogin/project/app/models.py
+++ RegisterLogin/project/app/models.py
@@ -6,7 +6,9 @@ class Student(models.Model):
     Email=models.EmailField()
     Contact=models.IntegerField()
     Details=models.CharField(max_length=100)
+    Password=models.CharField(max_length=20,null=True)
     Image=models.ImageField(upload_to='image/')
+   
 
     def __str__(self):
         return self.name +" "+self.email
--- RegisterLogin/project/app/views.py
+++ RegisterLogin/project/app/views.py
@@ -1,22 +1,38 @@
 from django.shortcuts import render
-
+from .models import Student
 # Create your views here.
 def landing(req):
     return render(req,'landing.html')
 def register(req):
     return render(req,'register.html')
 def registerdata(req):
+    print(req.POST)
     if req.method=='POST':
         n=req.POST.get('Name')
         e=req.POST.get('Email')
         c=req.POST.get('Contact')
         d=req.POST.get('Details')
         i=req.FILES.get('Image')
+        p=req.POST.get('Password')
+        cp=req.POST.get('Cpassword')
         print(n,e,c,d,i)
         user=Student.objects.filter(Email=e)
         if user:
-            msg="Email already exist"
+            msg="Already Exist"
             return render(req,'register.html',{'msg':msg})
         else:
-            Student.objects.Create(Name=n,Email=e,Contact=c,Details=d,Image=i)
-            return(req,'register.html',{'msg':"data save"})
+            if p==cp:
+                Student.objects.create(Name=n,Email=e,Contact=c,Details=d,Password=p)
+                msg = "Registration Done"
+                return render(req,'login.html',{'msg':msg})
+            else:
+                msg='p & cp not match'
+                return render(req,'register.html',{'msg':msg})
+
+
+        # if user:
+        #     msg="Email already exist"
+        #     return render(req,'register.html',{'msg':msg})
+        # else:
+        #     Student.objects.Create(Name=n,Email=e,Contact=c,Details=d,Image=i)
+        #     return(req,'register.html',{'msg':"data save"})
