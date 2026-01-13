7c042047843c93321b5704a465a0e3a531aaf054
san7842 <sandeepjjjjj536@gmail.com>
2026-01-13 19:32:49 +0530

    add

--- corepyapi/project/app/views.py
+++ corepyapi/project/app/views.py
@@ -40,6 +40,7 @@ def movies_list(req):
 @csrf_exempt   
 def movies_details(req,pk):
     if req.method=='PUT':
+    
         j_data=req.body
         # print(j_data)
         # print(type(j_data))
@@ -51,14 +52,19 @@ def movies_details(req,pk):
         # po_data['name']=n_data['name']
         # po_data['dis']=n_data['dis']
         # po_data['action']=n_data['action']
-        o_data.name=n_data['name']
-        o_data.dis=n_data['dis']
-        o_data.action=n_data['action']
-        o_data.save()
+        if (n_data.get('name',False) and n_data.get('dis',False) and n_data('active',False)):
+            o_data.name=n_data['name']
+            o_data.dis=n_data['dis']
+            o_data.active=n_data['active']
+            o_data.save()
         
-        d={'msg':"objects update successfully"}
-        jd=json.dumps(d)
-        return HttpResponse(jd,content_type='applications/json')
+            d={'msg':"objects update successfully"}
+            jd=json.dumps(d)
+            return HttpResponse(jd,content_type='applications/json')
+        else:
+              d={'msg':"objects not found"}
+              jd=json.dumps(d)
+              return HttpResponse(jd,content_type='applications/json')
     # elif req.method=='PATCH':
     #     pass
     # elif req.method=='DELETE':
@@ -71,3 +77,51 @@ def movies_details(req,pk):
     # print(type(p_data))
     # j_data=json.dumps(p_data)
     # return HttpResponse(j_data,content_type='application/json')
+    if req.method=='PATCH':
+        
+        j_data=req.body
+        # print(j_data)
+        # print(type(j_data))
+        n_data=json.loads(j_data)
+        # print(n_data)
+        # print(type(n_data))
+        o_data=Movies.objects.get(id=pk)
+        # po_data=model_to_dict(o_data)
+        # po_data['name']=n_data['name']
+        # po_data['dis']=n_data['dis']
+        # po_data['action']=n_data['action']
+
+        # if (n_data.get('name',False) and n_data.get('dis',False) and n_data('active',False)):
+        #     o_data.name=n_data['name']
+        #     o_data.dis=n_data['dis']
+        #     o_data.active=n_data['active']
+        #     o_data.save()
+        if n_data.get('name',False):
+            o_data.name=n_data['name']
+        if n_data.get('dis',False):
+            o_data.dis=n_data['dis']
+        if n_data.get('active',False):
+            o_data.active=n_data['active']
+        o_data.save()
+        
+        d={'msg':"objects partially  update successfully"}
+        jd=json.dumps(d)
+        return HttpResponse(jd,content_type='applications/json')
+        # else:
+        #       d={'msg':"objects not found"}
+        #       jd=json.dumps(d)
+        #       return HttpResponse(jd,content_type='applications/json')
+    if req.method=='DELETE':
+        id=Movies.objects.filter(id=pk)
+        if id:
+
+            data=Movies.objects.get(id=pk)
+            data.delete()
+            p_data={'msg':"object delete"}
+            j_data=json.dumps(p_data)
+            return HttpResponse(j_data,content_type='application/json')
+        p_data={'msg':"id not present in db"}
+        j_data=json.dumps(p_data)
+        return HttpResponse(j_data,content_type='application/json')
+    
+            
