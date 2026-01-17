3d462ddc2d49ee0ae05357330ef5e519f96ec71a
san7842 <sandeepjjjjj536@gmail.com>
2026-01-17 18:54:28 +0530

    add

--- serilizeranddeseriliser/project/app/serializers.py
+++ serilizeranddeseriliser/project/app/serializers.py
@@ -1,6 +1,16 @@
 from rest_framework import  serializers
+from .models import Student
 
 class Student1(serializers.Serializer):
     name=serializers.CharField()
     email=serializers.EmailField()
     city=serializers.CharField()
+
+    def create(self, validated_data):
+        return Student.objects.create(**validated_data)
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get('name', instance.name)
+        instance.email = validated_data.get('email', instance.email)
+        instance.city = validated_data.get('city', instance.city)
+        instance.save()
+        return instance
